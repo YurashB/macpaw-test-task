017c1085a3a7c8798debfab67ced1101f7d6e5e1
Bohdan Yurash <yurash.bogdan@gmail.com>
2023-08-24 16:59:06 +0300

    create models and migration

---
+++ database/migrations/2023_08_24_100928_create_collections_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('CREATE TABLE Collections (
+    id            BIGINT        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title         VARCHAR(255)  not null,
+    description   TEXT,
+    target_amount decimal(8, 2) NOT NULL,
+    link          TEXT          NOT NULL,
+    created_at    DATETIME      NOT NULL DEFAULT NOW()
+        )');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collections');
+    }
+};
---
+++ database/migrations/2023_08_24_101554_create_contributors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('CREATE TABLE Contributors (
+    id            BIGINT        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    collection_id BIGINT        NOT NULL,
+    user_name     VARCHAR(255)  NOT NULL,
+    amount        DECIMAL(8, 2) NOT NULL,
+    CONSTRAINT fk_collection FOREIGN KEY (collection_id)
+        REFERENCES Collections (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+        )');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contributors');
+    }
+};
